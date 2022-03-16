fd210e865e241e4e3ef66d3134e2570792171e44
Vlolsen90 <98793899+v-olsen@users.noreply.github.com>
2022-03-16 03:16:58 -0500

    sumAll first attempt

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
+const sumAll = function (beginSum, endSum) {
+  if (beginSum == null || endSum == null || beginSum < 0 || endSum < 0) {
+    return "ERROR"
+  }
 
-};
+  let adder = 0
+  if (endSum > beginSum) {
+    for (let i = endSum; i <= beginSum; i++) {
+      adder += i
+    }
+}
+else {
+    for (let i = beginSum; i <= endSum; i++) {
+        adder += i
+    }
+    return adder
+}
 
 // Do not edit below this line
-module.exports = sumAll;
+module.exports = sumAll
+
+//npm test sumAll.spec.js
