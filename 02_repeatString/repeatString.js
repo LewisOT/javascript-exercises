2baf4e5f5a8eb4fa5003f0bea631581c6ec53994
Vlolsen90 <98793899+v-olsen@users.noreply.github.com>
2022-03-15 21:41:55 -0500

    finished first exercise. working on second

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,12 @@
-const repeatString = function() {
-
-};
-
+const repeatString = function (string, num) {
+  let container = ""
+  if (num < 0) {
+    return "ERROR"
+  }
+  for (let i = 0; i < num; i++) {
+    container += string
+  }
+  return container
+}
 // Do not edit below this line
-module.exports = repeatString;
+smodule.exports = repeatString
