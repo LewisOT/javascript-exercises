30979f3a125a81f0cdbb8898117a733866a279e6
Vlolsen90 <98793899+v-olsen@users.noreply.github.com>
2022-03-17 02:06:10 -0500

    finished tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,30 @@
-const ftoc = function() {
+const ftoc = function (fahrenheit) {
+  //(32°F − 32) × 5/9
+  let celsius = (fahrenheit - 32) * (5 / 9)
+  if (celsius == 0) {
+    return 0
+  } else {
+    return Number(celsius.toFixed(1))
+  }
+}
 
-};
+const ctof = function (celsius) {
+  //(0°C × 9/5) + 32
 
-const ctof = function() {
+  let fahrenheit = celsius * 1.8 + 32
 
-};
+  if (fahrenheit == 0) {
+    return 0
+  } else {
+    return Number(fahrenheit.toFixed(1))
+  }
+}
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
-};
+  ctof,
+}
+
+// use number.toFixed(1)
+// npm test tempConversion.spec.js
