0bee06efe1f2bcaadf0d9d4f4bfd60907da823d0
LewisOT <91518540+LewisOT@users.noreply.github.com>
2022-05-01 02:41:09 -0500

    finished calculator and palindromes

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,49 @@
-const add = function() {
-	
-};
-
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
+const add = function(a,b) {
+  return a+b
+    
+  };
+  
+  const subtract = function(a,b) {
+    return a-b
+  };
+  
+  const sum = function(a) {
+    let cup = 0;
+    for (let i = 0; i < a.length; i++) {
+      cup += a[i]
+    }
+    return cup
+    
+  };
+  
+  const multiply = function(a) {
+    let cup = 1;
+    console.log("initial: " + cup)
+    for (let i = 0; i < a.length; i++) {
+      console.log("increment: " + i)
+      console.log("index: " + a[i])
+      cup *= a[i]
+      console.log("increment results: " + cup)
+    }
+    return cup
+  };
+  console.log(multiply([2,4]))
+  
+  
+  
+  const power = function(a,b) {
+    return a**b
+    
+  };
+  
+  const factorial = function(a) {
+    let cup = 1;
+    for (let i = 1 ; i <= a; i++) {
+      cup *= i
+    }
+    return cup
+    
+  };
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+function palindromes(string) {
+    let dataStorage = string.toLowerCase();
+    let storage = ""
+    let storageTwo = "";
+    for (let i = 0; i < dataStorage.length; i++) {
+        console.log(dataStorage[i])
+        if (dataStorage.match(/[A-Za-z]/) && dataStorage[i] !== " " && dataStorage[i] !== "," && dataStorage[i] !== ";" && dataStorage[i] !== "!" && dataStorage[i] !== ".") {
+            console.log('passed logic')
+            storage += dataStorage[i]
+            storageTwo += dataStorage[i]
+            console.log("this is the storage " + storage)
+            console.log(storage)
+        }
+    }
 
+
+    storage = [...storage].reverse().join("")
+
+    if (storage === storageTwo){
+        return true
+
+    }
+    console.log(storage)
+    return false
 };
-
+console.log(palindromes('Racecar!'))
 // Do not edit below this line
 module.exports = palindromes;
+
