fd08e90f851db0025835ca9565c877943851b2d1
LewisOT <91518540+LewisOT@users.noreply.github.com>
2022-05-02 02:58:37 -0500

    completed fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(array) {
+    if (array < 0) {
+        return "OOPS"
+    }
+    array = parseInt(array)
+    let storage = [0, 1, 1];
+    let tempBin = 0;
+    for (let i = 0; i <= array-3; i++){
+        for (let j = 1; j < storage.length; j++) {
 
+            tempBin += storage[j]
+        }
+        storage.shift();
+        storage.push(tempBin)
+        tempBin = 0;
+    }
+return storage[2]
 };
 
 // Do not edit below this line
